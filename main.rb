5df5a8cd0db13b0d660546ed98c0b3d7f8d976d6
Keishake <keishake0925@gmail.com>
2015-12-09 11:07:52 +0900

    led tikatika

--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'pi_piper'
 set :bind, '0.0.0.0'
+
+boolkun = false
+led = PiPiper::Pin.new pin:20, direction: :out
 get '/' do
+  if boolkun
+    led.on
+  else
+    led.off
+  end
+  boolkun = !boolkun
   "Hello I'm Raspberry Pi!!!!"
 end
