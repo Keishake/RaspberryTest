434afa0e53c89f06e7597f0951c3d1c182919237
Keishake <keishake0925@gmail.com>
2015-12-09 12:27:33 +0900

    fix bug

--- main.rb
+++ main.rb
@@ -1,14 +1,11 @@
 require 'sinatra'
 require 'pi_piper'
 set :bind, '0.0.0.0'
-at_exit {
-  uexp = open("/sys/class/gpio/unexport", "w")
-  uexp.write(20)
-  uexp.close
-}
+
 boolkun = false
-led = PiPiper::Pin.new pin:20, direction: :out
+
 get '/' do
+  led = PiPiper::Pin.new pin:20, direction: :out
   if boolkun
     led.on
   else
